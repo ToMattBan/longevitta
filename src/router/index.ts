2a7a75df161be79e8fe6481568a38363310d5d4a
ToMattBan <souaquelecleyton@yandex.com>
2025-08-11 19:45:22 -0300

    Created login page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import login from '@/pages/login.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: "/", component: login, alias: '/login' },
+  ],
 })
 
 export default router

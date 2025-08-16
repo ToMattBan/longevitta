47d9acdfad68d1a9373d1a26749ce6bd706af9a7
ToMattBan <souaquelecleyton@yandex.com>
2025-08-16 19:29:24 -0300

    Small login validation

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -6,12 +6,18 @@
 
     <div class="input-container">
       <label>Nome:</label>
-      <input required />
+      <input required v-model="name" />
+      <div class="login-error" v-if="showErrorName">
+        Para acessar, utilize o nome "Alfredo"
+      </div>
     </div>
 
     <div class="input-container">
       <label>Senha:</label>
-      <input type="password" required />
+      <input type="password" required v-model="pass" />
+      <div class="login-error" v-if="showErrorPass">
+        Para acessar, utilize a senha "1234"
+      </div>
     </div>
 
     <LongeButton variant="secondary" type="submit">Entrar</LongeButton>
@@ -21,14 +27,27 @@
 </template>
 
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
+import { ref } from 'vue';
+
 import LogoHeader from '@/components/LogoHeader.vue';
 import LongeButton from '@/components/LongeButton.vue';
-import { useRouter } from 'vue-router';
 
 const router = useRouter()
 
+const name = ref<string>('');
+const pass = ref<string>('');
+
+const showErrorName = ref<boolean>(false)
+const showErrorPass = ref<boolean>(false)
+
 function enterApp() {
-  router.push('/home')
+  let canLogin = true;
+
+  if (name.value !== 'Alfredo') { showErrorName.value = true; canLogin = false }
+  if (pass.value !== '1234') { showErrorPass.value = true; canLogin = false }
+
+  if (canLogin) router.push('/home');
 }
 </script>
 
@@ -62,6 +81,13 @@ form {
       border: 2px solid var(--color-primary);
       height: 40px;
       padding: 8px 16px;
+    }
+
+    .login-error {
+      color: red;
+      font-weight: normal;
+      font-size: 0.8rem;
+      margin-top: 4px;
     }
   }
 
